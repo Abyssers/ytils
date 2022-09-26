6555e5ec8ab9b1a48d42baef821c977665200b48
Morilence <1280659615@qq.com>
2022-09-26 20:19:29 +0800

    feat: defined type checking methods 4 Map & WeakMap

---
+++ src/type/isMap.ts
@@ -0,0 +1,15 @@
+/**
+ * Checks whether value is a Map object.
+ * @param {any} value The value to check
+ * @returns {boolean} Returns true if value is a Map object, else false
+ * @example
+ *
+ * isMap(new Map())
+ * // => true
+ *
+ * isMap(new Map([[0, 1], [1, 2]]))
+ * // => true
+ */
+export function isMap(value: any): boolean {
+    return Object.prototype.toString.call(value) === "[object Map]";
+}
---
+++ src/type/isWeakMap.ts
@@ -0,0 +1,15 @@
+/**
+ * Checks whether value is a WeakMap object.
+ * @param {any} value The value to check
+ * @returns {boolean} Returns true if value is a WeakMap object, else false
+ * @example
+ *
+ * isWeakMap(new WeakMap())
+ * // => true
+ *
+ * isWeakMap(new WeakMap([[{ n: "a" }, 1], [{ n: "b" }, 2]]))
+ * // => true
+ */
+export function isWeakMap(value: any): boolean {
+    return Object.prototype.toString.call(value) === "[object WeakMap]";
+}
