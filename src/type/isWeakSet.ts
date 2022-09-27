585ff2c83b180dede7b548435542975cc96e0b8e
Morilence <1280659615@qq.com>
2022-09-28 00:44:06 +0800

    feat: added isWeakSet method

---
+++ src/type/isWeakSet.ts
@@ -0,0 +1,17 @@
+import { tagOf, TypeTag } from "../core/tag";
+
+/**
+ * Checks whether value is a WeakSet object.
+ * @param {any} value The value to check
+ * @returns {boolean} Returns true if value is a WeakSet object, else false
+ * @example
+ *
+ * isWeakSet(new WeakSet())
+ * // => true
+ *
+ * isWeakSet(new WeakSet([{ a: 1 }, { b: 2 }]))
+ * // => true
+ */
+export function isWeakSet(value: any): boolean {
+    return tagOf(value) === TypeTag.WeakSet;
+}
