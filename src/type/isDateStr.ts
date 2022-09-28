d347c50b3ec406d04f66320bc96e0533c35ce003
Morilence <1280659615@qq.com>
2022-09-28 19:40:38 +0800

    feat: added isDateStr method

---
+++ src/type/isDateStr.ts
@@ -0,0 +1,27 @@
+import { tagOf, TypeTag } from "../core/tag";
+import { isNum } from "./isNum";
+
+/**
+ * Checks whether value is a valid date-string or a String object that can be transformed to a valid date.
+ * @param {any} value The value to check
+ * @returns {boolean} Returns true if value is a valid date-string or a String object that can be transformed to a valid date, else false
+ * @example
+ *
+ * isDateStr("Thu Nov 10 2022 00:00:00 GMT+0800")
+ * // => true
+ *
+ * isDateStr("2022 11 10")
+ * // => true
+ *
+ * isDateStr("2022/11/10")
+ * // => true
+ *
+ * isDateStr("20221110")
+ * // => false
+ *
+ * isDateStr("1234567")
+ * // => false
+ */
+export function isDateStr(value: any): boolean {
+    return tagOf(value) === TypeTag.String && isNum(new Date(value).getTime());
+}
