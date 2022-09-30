ee02054330eefdfbc6c17fb7c50786ecbdfd3389
Morilence <1280659615@qq.com>
2022-09-30 19:47:47 +0800

    feat: added isAnonFunc, isGenFunc, isAsyncGenFunc, isGen and isAsynGen methods

---
+++ src/type/isAsyncGenFunc.ts
@@ -0,0 +1,26 @@
+import { tagOf, TypeTag } from "../.core/tag";
+
+/**
+ * Checks whether value is an AsyncGeneratorFunction object.
+ * @param {any} value The value to check
+ * @returns {boolean} Returns true if value is an AsyncGeneratorFunction object, else false
+ * @example
+ *
+ * isAsyncGenFunc(async function* () {})
+ * // => true
+ *
+ * isAsyncGenFunc(function* () {})
+ * // => false
+ *
+ * isAsyncGenFunc(function () {})
+ * // => false
+ *
+ * isAsyncGenFunc(() => {})
+ * // => false
+ *
+ * isAsyncGenFunc("6")
+ * // => false
+ */
+export function isAsyncGenFunc(value: any): boolean {
+    return tagOf(value) === TypeTag.AsyncGeneratorFunction;
+}
