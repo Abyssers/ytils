c18f9ae00b142e134851f1fb7622b8f48c17394e
Morilence <1280659615@qq.com>
2022-10-08 12:02:29 +0800

    build: supported on-demand introduction (tree shaking)

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
 const json = require("@rollup/plugin-json");
 const typescript = require("@rollup/plugin-typescript");
 const strip = require("@rollup/plugin-strip");
+const { terser } = require("rollup-plugin-terser");
 
 module.exports = {
     input: "src/index.ts",
-    output: {
-        file: "dist/index.js",
-        format: "cjs",
-    },
-    plugins: [json(), typescript(), strip({ debugger: true })],
+    output: [
+        {
+            file: "dist/index.js",
+            format: "cjs",
+        },
+        {
+            file: "dist/index.esm.js",
+            format: "esm",
+        },
+    ],
+    plugins: [json(), typescript(), strip({ debugger: true }), terser()],
 };
