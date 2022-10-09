ff9f349af948e0b1760701d27172c828850bd972
Morilence <1280659615@qq.com>
2022-10-09 13:47:37 +0800

    perf: unified the rules of isEq and cloneOf

---
+++ src/recreation/cloneOf.ts
@@ -0,0 +1,47 @@
+import { tagOf, TypeTag } from "../.internal/tag";
+import { isFunc } from "../type/isFunc";
+
+/**
+ * Gets the clone of value
+ * @param {any} value The value to be cloned
+ * @returns The clone of value
+ */
+export function cloneOf(value: any): any {
+    if (value == null) return value;
+    const set = new WeakSet();
+    return clone(value);
+
+    function clone(value: any) {
+        if (set.has(value)) {
+            return value;
+        }
+        const tag = tagOf(value);
+        if (tag === TypeTag.Number || tag === TypeTag.Boolean || tag === TypeTag.String)
+            return typeof value !== "object" ? value : new value.constructor(value);
+        if (
+            tag === TypeTag.Date ||
+            tag === TypeTag.Error ||
+            tag === TypeTag.Map ||
+            tag === TypeTag.Set ||
+            tag === TypeTag.WeakMap ||
+            tag === TypeTag.WeakSet
+        )
+            return new value.constructor(value);
+        if (tag === TypeTag.RegExp) {
+            const clone = new RegExp(value);
+            clone.lastIndex = (value as RegExp).lastIndex;
+            return clone;
+        }
+        if (tag === TypeTag.Symbol || isFunc(value)) return value;
+        if (tag === TypeTag.Array) return value.map((item: any) => cloneOf(item));
+        if (typeof tag === "object") {
+            const clone = Object.create(Object.getPrototypeOf(value));
+            const keys = Object.keys(value);
+            for (let i = keys.length - 1; i >= 0; i--) {
+                clone[keys[i]] = clone(value[keys[i]]);
+            }
+            return clone;
+        }
+        return null;
+    }
+}
