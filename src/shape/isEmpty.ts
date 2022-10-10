d758c1cdb89edd8f425f009f67701b48824962f7
Morilence <1280659615@qq.com>
2022-10-10 10:51:46 +0800

    perf: removed TypedArray because ArrayLike already contains it

--- src/shape/isEmpty.ts
+++ src/shape/isEmpty.ts
@@ -1,5 +1,4 @@
 import { isArrLike } from "./isArrLike";
-import { isTypedArr } from "../type/isTypedArr";
 import { tagOf, TypeTag } from "../.internal/tag";
 
 /**
@@ -32,14 +31,12 @@ import { tagOf, TypeTag } from "../.internal/tag";
  * // => false
  */
 export function isEmpty(value: object): boolean;
-export function isEmpty(value: TypedArray): boolean;
 export function isEmpty<T>(value: ArrayLike<T>): boolean;
 export function isEmpty<T>(value: Set<T>): boolean;
 export function isEmpty<T, U>(value: Map<T, U>): boolean;
-export function isEmpty<T, U>(value: object | TypedArray | ArrayLike<T> | Set<T> | Map<T, U>): boolean {
+export function isEmpty<T, U>(value: object | ArrayLike<T> | Set<T> | Map<T, U>): boolean {
     if (value == null) return true;
     if (isArrLike(value)) return (value as ArrayLike<T>).length === 0;
-    if (isTypedArr(value)) return (value as TypedArray).byteLength === 0;
     if (tagOf(value) === TypeTag.Map || tagOf(value) === TypeTag.Set) return (value as Set<T> | Map<T, U>).size === 0;
     if (typeof value === "object") return Object.keys(value).length === 0;
     return true;
