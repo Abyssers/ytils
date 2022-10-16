a77247d55cc5f3eb750d644fbe980169dd10b990
NewVo1d <89888306+NewVo1d@users.noreply.github.com>
2022-10-16 23:37:28 +0800

    test: added unit tests for isArrBuffer, isAsyncGen, isAsyncGenFunc and isBigInt (#63)
    * test: added unit tests for isArrBuffer, isAsyncGen, isAsyncGenFunc and isBigInt
    
    * test: corrected some unit tests
    
    * test: standardized some unit tests

---
+++ packages/typ/src/native/__tests__/isAsyncGenFunc.test.ts
@@ -0,0 +1,57 @@
+import { isAsyncGenFunc } from "../isAsyncGenFunc";
+
+describe("isAsyncGenFunc", () => {
+    test("function", () => {
+        expect(
+            isAsyncGenFunc(async function* () {
+                /* do something */
+            })
+        ).toBeTruthy();
+
+        expect(isAsyncGenFunc(console.log)).toBeFalsy();
+        expect(
+            isAsyncGenFunc(function () {
+                /* do something */
+            })
+        ).toBeFalsy();
+        expect(
+            isAsyncGenFunc(() => {
+                /* do something */
+            })
+        ).toBeFalsy();
+        expect(
+            isAsyncGenFunc(function* () {
+                /* do something */
+            })
+        ).toBeFalsy();
+        expect(
+            isAsyncGenFunc(async function () {
+                /* do something */
+            })
+        ).toBeFalsy();
+        expect(
+            isAsyncGenFunc(async () => {
+                /* do something */
+            })
+        ).toBeFalsy();
+    });
+
+    test("other types", () => {
+        expect(isAsyncGenFunc(undefined)).toBeFalsy();
+        expect(isAsyncGenFunc(null)).toBeFalsy();
+        expect(isAsyncGenFunc(6)).toBeFalsy();
+        expect(isAsyncGenFunc(new Number(6.6))).toBeFalsy();
+        expect(isAsyncGenFunc("6")).toBeFalsy();
+        expect(isAsyncGenFunc(new String(6.6))).toBeFalsy();
+        expect(isAsyncGenFunc(Symbol(6))).toBeFalsy();
+        expect(isAsyncGenFunc({})).toBeFalsy();
+        expect(isAsyncGenFunc(new Object())).toBeFalsy();
+        expect(isAsyncGenFunc([])).toBeFalsy();
+        expect(isAsyncGenFunc(new Array(6))).toBeFalsy();
+        expect(isAsyncGenFunc(new RegExp("6"))).toBeFalsy();
+        expect(isAsyncGenFunc(new Map())).toBeFalsy();
+        expect(isAsyncGenFunc(new WeakMap())).toBeFalsy();
+        expect(isAsyncGenFunc(new Set())).toBeFalsy();
+        expect(isAsyncGenFunc(new WeakSet())).toBeFalsy();
+    });
+});
